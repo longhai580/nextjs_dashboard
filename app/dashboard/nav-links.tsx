e84b7ea7d2a614e13e4bf7898a8c89c133b3e3f7
Nguyen Long Hai <nguyenhai3266@gmail.com>
2024-09-17 16:23:04 +0700

    Updated layout, page, and added new dashboard and styles

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -10,7 +10,16 @@ import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
   import Link from 'next/link';
    
-  // ...
+  const links = [
+    { name: 'Home', href: '/dashboard', icon: HomeIcon },
+    {
+      name: 'Invoices',
+      href: '/dashboard/invoices',
+      icon: DocumentDuplicateIcon,
+    },
+    { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  ];
+  
    
 export default function NavLinks() {
     const pathname = usePathname();
